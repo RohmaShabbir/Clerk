a6d4df1bd14abd5c8e4e2346a4b758e0cda05192
Rohma-Shabbir <168419231+RohmaShabbir@users.noreply.github.com>
2024-12-15 23:17:39 +0500

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "./components/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Clerk App ",
+  description: "Example Clerk App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Header/>
+          <main className="container ">
+          <div className="flex items-start justify-center min-h-screen">
+          <div className="mt-20">
+          {children}
+          </div>
+          </div> 
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <h1 className='text-2xl font-bold mb-5 text-center text-gray-800'>
+        Welcome
+      </h1>
+      <p className="mb-5 text-center text-gray-600">
+      "This is a demo site that I have created for the Sign Up and Sign In features using <b>Next.js</b> and <b>Clerk</b>. 
+       <br/>Go ahead and sign up or sign in!"
+      </p>
+    </>
+  )
+}
+
+export default Home
